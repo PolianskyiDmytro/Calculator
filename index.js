794921a9fa565d068b1751ade6546f0b8d6ccc26
Polianskyi Dmytro <dimdemon1@gmail.com>
2024-05-27 22:39:50 +0300

    complete addToDisplay function

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const display = document.getElementById("display");
 
 function addToDisplay(input){
+  if (!'1234567890'.includes(display.value[display.value.length - 1]) && (input === '.' || input === '-' || input === '+' || input === '*' || input === '/')){
+    event.preventDefault();
+    return
+  }
+  if(display.value.includes('.') && input === '.'){
+    event.preventDefault();
+    return
+  }
+
+  if(display.value.includes('-') && (input === '-' || input === '+' || input === '*' || input === '/')){
+    event.preventDefault();
+    return
+  }
+
+  if(display.value.includes('+') && (input === '-' || input === '+' || input === '*' || input === '/')){
+    event.preventDefault();
+    return
+  }
+
+  if(display.value.includes('*') && (input === '-' || input === '+' || input === '*' || input === '/')){
+    event.preventDefault();
+    return
+  }
+
+  if(display.value.includes('/') && (input === '-' || input === '+' || input === '*' || input === '/')){
+    event.preventDefault();
+    return
+  }
+
   display.value += input;
   event.preventDefault();
 }
