b5b21cfc5289cd5b174ef87337784b200e1c573d
Polianskyi Dmytro <dimdemon1@gmail.com>
2024-05-28 15:14:01 +0300

    complete addToDisplay function

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const display = document.getElementById("display");
+let dotSwitch = true;
 
 function addToDisplay(input){
-  if (!'1234567890'.includes(display.value[display.value.length - 1]) && (input === '.' || input === '-' || input === '+' || input === '*' || input === '/')){
-    event.preventDefault();
-    return
-  }
-  if(display.value.includes('.') && input === '.'){
-    event.preventDefault();
-    return
-  }
-
-  if(display.value.includes('-') && (input === '-' || input === '+' || input === '*' || input === '/')){
+  if (!'1234567890'.includes(display.value[display.value.length - 1]) 
+  && (input === '.' || input === '-' || input === '+' || input === '*' || input === '/')){
     event.preventDefault();
     return
   }
 
-  if(display.value.includes('+') && (input === '-' || input === '+' || input === '*' || input === '/')){
+  if('1234567890'.includes(display.value[display.value.length - 1]) 
+  && input === '.' 
+  && (display.value.includes('-') || display.value.includes('+') || display.value.includes('*') || display.value.includes('/'))){
+    if (dotSwitch === false){
+      event.preventDefault();
+      return
+    }
+    dotSwitch = false;
+    display.value += input;
     event.preventDefault();
     return
   }
 
-  if(display.value.includes('*') && (input === '-' || input === '+' || input === '*' || input === '/')){
+  if (display.value.includes('.') && input === '.'){
     event.preventDefault();
     return
   }
 
-  if(display.value.includes('/') && (input === '-' || input === '+' || input === '*' || input === '/')){
+  if((display.value.includes('-') || display.value.includes('+') || display.value.includes('*') || display.value.includes('/')) 
+  && (input === '-' || input === '+' || input === '*' || input === '/')){
     event.preventDefault();
     return
   }
