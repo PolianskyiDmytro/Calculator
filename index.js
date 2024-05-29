23bf4492c7793b0d5af5443d017a584174d527d7
Polianskyi Dmytro <dimdemon1@gmail.com>
2024-05-29 16:43:35 +0300

    add calculate function

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const display = document.getElementById("display");
 let dotSwitch = true;
 
-function addToDisplay(input){
-  if (!'1234567890'.includes(display.value[display.value.length - 1]) 
-  && (input === '.' || input === '-' || input === '+' || input === '*' || input === '/')){
+function addToDisplay(input) {
+  if (!'1234567890'.includes(display.value[display.value.length - 1])
+    && (input === '.' || input === '-' || input === '+' || input === '*' || input === '/')) {
     event.preventDefault();
     return
   }
 
-  if('1234567890'.includes(display.value[display.value.length - 1]) 
-  && input === '.' 
-  && (display.value.includes('-') || display.value.includes('+') || display.value.includes('*') || display.value.includes('/'))){
-    if (dotSwitch === false){
+  if ('1234567890'.includes(display.value[display.value.length - 1])
+    && input === '.'
+    && (display.value.includes('-') || display.value.includes('+') || display.value.includes('*') || display.value.includes('/'))) {
+    if (dotSwitch === false) {
       event.preventDefault();
       return
     }
@@ -21,13 +21,13 @@ function addToDisplay(input){
     return
   }
 
-  if (display.value.includes('.') && input === '.'){
+  if (display.value.includes('.') && input === '.') {
     event.preventDefault();
     return
   }
 
-  if((display.value.includes('-') || display.value.includes('+') || display.value.includes('*') || display.value.includes('/')) 
-  && (input === '-' || input === '+' || input === '*' || input === '/')){
+  if ((display.value.includes('-') || display.value.includes('+') || display.value.includes('*') || display.value.includes('/'))
+    && (input === '-' || input === '+' || input === '*' || input === '/')) {
     event.preventDefault();
     return
   }
@@ -36,11 +36,25 @@ function addToDisplay(input){
   event.preventDefault();
 }
 
-function clearDisplay(){
+function clearDisplay() {
+  if (display.value[display.value.length - 1] === '.') {
+    dotSwitch = true;
+  }
+  if (display.value === "Error"){
+    display.value = "";
+    event.preventDefault()
+  }
   display.value = display.value.slice(0, -1);
   event.preventDefault();
 }
 
-function calculate(){
-
+function calculate() {
+  try {
+    display.value = eval(display.value);
+    event.preventDefault();
+  }
+  catch(error){
+    display.value = "Error";
+    event.preventDefault();
+  }
 }
